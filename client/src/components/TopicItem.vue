5ea87b04141d075a1b0ba399248b0d44e24c25bb
Robert Templeton <roberttee1993@googlemail.com>
2019-03-01 17:49:45 +0000

    added handleclick function to topicItem and started TopicDetail.vue

--- client/src/components/TopicItem.vue
+++ client/src/components/TopicItem.vue
@@ -1,16 +1,19 @@
 <template lang="html">
-  <div>
+  <div v-on:click="handleClick">
     <h2>{{ item.species }}</h2>
     <img :src="item.picture" alt="no image available">
   </div>
 </template>
 
 <script>
+import { eventBus } from "../main.js"
 export default {
   name: "TopicItem",
   props: ["item"],
   methods(){
-
+    handleClick(){
+      eventBus.$emit("item-clicked", item)
+    }
   }
 
 }
