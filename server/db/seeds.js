31dd3d0a0405d057a8958277ae51abd82615164d
Robert Templeton <roberttee1993@googlemail.com>
2019-03-01 15:01:23 +0000

    added some more data to seeds file

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -7,52 +7,52 @@ db.sharks.insertMany([
     diet: "Prey upon fish (e.g. tuna, rays, other sharks), cetaceans (i.e., dolphins, porpoises, whales), pinnipeds (e.g. seals, fur seals, and sea lions), sea turtles, sea otters and seabirds.",
     population: 3500,
     size: "Female: 4.5 – 6.4 m (Adult), Male: 3.5 – 4 m (Adult)",
-    picture:,
+    picture: "",
     location: "USA, South Africa, Japan, Oceania, Chile, and the Mediterranean",
     Fact: "The great white is at the top of the food chain and has few threats in the ocean."
   },
   {
     species: "Great Hammerhead",
     diet: "Fish (including other sharks), squid, octopus, and crustaceans",
-    population:,
+    population: 0,
     size: "6m",
-    picture:,
+    picture: "",
     location: "Colombia, Costa Rica (Cocos Island), and Hawaii.",
     Fact: "The Great Hammerhead weighs about the same as a piano."
   },
   {
     species: "Tiger",
     diet: "",
-    population:,
+    population: 0,
     size: "",
-    picture:,
+    picture: "",
     location: "",
     Fact: ""
   },
   {
     species: "Whale",
     diet: "",
-    population:,
+    population: 0,
     size: "",
-    picture:,
+    picture: "",
     location: "",
     Fact: ""
   },
   {
     species: "Bull",
     diet: "",
-    population:,
+    population: 0,
     size: "",
-    picture:,
+    picture: "",
     location: "",
     Fact: ""
   },
   {
     species: "Blue",
     diet: "",
-    population:,
+    population: 0,
     size: "",
-    picture:,
+    picture: "",
     location: "",
     Fact: ""
     }
